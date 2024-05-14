9cd535fb5fecc7a1343b97498d45a73637b1df6d
Breno Nogueira Araújo <breno.nog.araujo@hotmail.com>
2024-05-13 21:31:51 -0400

    feat: implement add match route

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,31 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:meus_gols_app/ui/goals/goals.dart';
-import 'package:meus_gols_app/ui/matches_screen.dart';
+import 'package:meus_gols_app/ui/screens/add_match.dart';
+import 'package:meus_gols_app/ui/screens/goals.dart';
+import 'package:meus_gols_app/ui/screens/matches_screen.dart';
 
 class Routes extends StatelessWidget {
   Routes({super.key});
 
-/// The route configuration.
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const Goals();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'matches',
-          builder: (BuildContext context, GoRouterState state) {
-            return const MatchesScreen();
-          },
-        ),
-      ],
-    ),
-  ],
-);
-
+  /// The route configuration.
+  final GoRouter _router = GoRouter(
+    routes: <RouteBase>[
+      GoRoute(
+        path: '/',
+        builder: (BuildContext context, GoRouterState state) {
+          return const Goals();
+        },
+        routes: <RouteBase>[
+          GoRoute(
+            name: 'Matches',
+            path: 'matches',
+            builder: (BuildContext context, GoRouterState state) {
+              return const MatchesScreen();
+            },
+          ),
+           GoRoute(
+            name: 'Add Match',
+            path: 'add_match',
+            builder: (BuildContext context, GoRouterState state) {
+              return const AddMatch();
+            },
+          ),
+        ],
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
